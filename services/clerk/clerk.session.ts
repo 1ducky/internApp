bab8b29ea77480d0e05890e8063505b48f402bf4
Real Hadyan <realhadyanfico@gmail.com>
2026-06-12 12:31:22 +0700

    fix:typo authorized

--- services/clerk/clerk.session.ts
+++ services/clerk/clerk.session.ts
@@ -6,8 +6,8 @@ export type ClerkSession = {
     userId: string,
     email: string,
     role: string
-    img?:string
-    name?:string
+    img?: string
+    name?: string
 }
 
 export type UserPublicMetadata = {
@@ -15,31 +15,31 @@ export type UserPublicMetadata = {
     role: string
 }
 
-export async function getAuthSessionClerk() : Promise<ClerkSession | undefined> {
+export async function getAuthSessionClerk(): Promise<ClerkSession | undefined> {
     const user = await currentUser()
-    if(!user) return undefined
+    if (!user) return undefined
     const externalAccountGoogle = user.externalAccounts.find((account) => account.provider === 'oauth_google')
     const fullname = externalAccountGoogle?.firstName + ' ' + externalAccountGoogle?.lastName
     const metadata = user?.publicMetadata as UserPublicMetadata
-    if(!metadata.role && !metadata.id){
+    if (!metadata.role && !metadata.id) {
         const publicData = await userRepository.userInitializeSession(user.id)
-        if(publicData.data && publicData.success){
+        if (publicData.data && publicData.success) {
             return {
                 userClerkId: user.id,
                 userId: publicData.data.id,
                 email: user.emailAddresses[0].emailAddress,
                 role: publicData.data.role,
-                img:user.imageUrl,
-                name:publicData.data.name ?? user.username ?? fullname
+                img: user.imageUrl,
+                name: publicData.data.name ?? user.username ?? fullname
             }
-        } 
+        }
     }
     return {
         userClerkId: user.id,
         userId: metadata.id,
         email: user.emailAddresses[0].emailAddress,
         role: metadata.role,
-        img:user.imageUrl,
+        img: user.imageUrl,
         name: user.username ?? fullname
     }
 }
